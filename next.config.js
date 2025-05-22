b8fac542c985d7fcb70e4cb8a7e836fd1a00b7f5
leulsm <leulsolm19@gmail.com>
2025-05-22 16:47:54 +0300

    some change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
